2fc150a35cceb24d376cf2619e9274a51cd4512c
baqihg <541464750@qq.com>
2016-12-30 17:35:25 +0800

    add Second count
    添加wxTimerSecond作为秒数倒计时

--- wxTimer.js
+++ wxTimer.js
@@ -1,32 +1,33 @@
 let intervarID;
 function wxTimer(beginTime="00:00:00",fn,interval=1,intervalFn){
     var getTime = new Date("2011/01/01 "+beginTime);
-    //初始化定时器
-    this.setData({
-        wxTimer:beginTime.length > 5 ? beginTime : beginTime+":00"
-    });
 
     //开始倒计时
     var that = this;
     
     var count = 0;
     //倒计时定时器
-    intervarID = setInterval(function(){
-        var tmpTime = new Date(getTime.getTime()-1000*count++).toString().substr(16,8);
+    function a(){
+        
+        var tmpTime = new Date(getTime.getTime()-1000*count++);
+        var tmpTimeStr = tmpTime.toString().substr(16,8);
         that.setData({
-            wxTimer:tmpTime
+            wxTimer:tmpTimeStr,
+            wxTimerSecond:(tmpTime.getTime() - new Date("2011/01/01 00:00:00").getTime()) / 1000
         });
         //计时结束
-        if(tmpTime == "00:00:00"){
+        if( 0 == (count-1) % interval && intervalFn){
+            intervalFn();
+        }
+        if(tmpTimeStr == "00:00:00"){
             if(fn){
                 fn();
             }
             stop();
         }
-        if( 0 == (count-1) % interval && intervalFn){
-            intervalFn();
-        }
-    },1000);
+    }
+    a();
+    intervarID = setInterval(a,1000);
 }
 
 function stop(){
@@ -35,4 +36,5 @@ function stop(){
 
 module.exports = {
     wxTimer: wxTimer,
+    stop:stop
 };
