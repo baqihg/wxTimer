07dfc355d2d67e765f321096f8e20d8f50f9c35b
baqihg <541464750@qq.com>
2017-03-02 16:45:36 +0800

    Update wxTimer.js

--- wxTimer.js
+++ wxTimer.js
@@ -1,40 +1,42 @@
-let intervarID;
-function wxTimer(beginTime="00:00:00",fn,interval=1,intervalFn){
-    var getTime = new Date("2011/01/01 "+beginTime);
-
-    //开始倒计时
-    var that = this;
-    
-    var count = 0;
-    //倒计时定时器
-    function a(){
-        
-        var tmpTime = new Date(getTime.getTime()-1000*count++);
-        var tmpTimeStr = tmpTime.toString().substr(16,8);
-        that.setData({
-            wxTimer:tmpTimeStr,
-            wxTimerSecond:(tmpTime.getTime() - new Date("2011/01/01 00:00:00").getTime()) / 1000
-        });
-        //计时结束
-        if( 0 == (count-1) % interval && intervalFn){
-            intervalFn();
-        }
-        if(tmpTimeStr == "00:00:00"){
-            if(fn){
-                fn();
-            }
-            stop();
-        }
-    }
-    a();
-    intervarID = setInterval(a,1000);
+var wxtimer = function (initObj){
+	initObj = initObj || {};
+	this.beginTime = initObj.beginTime || "00:00:00";
+	this.interval = initObj.interval || 0;
+	this.complete = initObj.complete;
+	this.intervalFn = initObj.intervalFn;
 }
 
-function stop(){
-    clearInterval(intervarID);
+wxtimer.prototype = {
+	start:function(self){
+		var getTime = new Date("2011/01/01 "+this.beginTime);//2011年1月1日的00：00：00的ms数
+	    //开始倒计时
+	    var that = this;
+	    var count = 0;//这个count在这里应该是表示s数，js中获得时间是ms，所以下面*1000都换成ms
+	    function begin(){
+	        var tmpTime = new Date(getTime.getTime()-1000*count++);
+	        //getTime.getTime()把2011年1月1日日 00：00：00换成字符串型字符串型，这样就可以直接1s，1s的减，就变成了倒计时，为了看的更明确，游泳new date把字符串换回来了
+	        var tmpTimeStr = tmpTime.toString().substr(16,8);//去掉前面的年月日就剩时分秒了
+	        self.setData({
+	            wxTimer:tmpTimeStr,
+	            wxTimerSecond:(tmpTime.getTime() - new Date("2011/01/01 00:00:00").getTime()) / 1000
+	        });
+	        
+	        //计时结束
+	        if( 0 == (count-1) % that.interval && that.intervalFn){
+	            that.intervalFn();
+	        }
+	        if(tmpTimeStr == "00:00:00"){
+	            if(that.complete){
+	                that.complete();
+	            }
+	            that.stop();
+	        }
+	    }
+	    begin();
+	    this.intervarID = setInterval(begin,1000);
+	},
+	stop:function(){
+		clearInterval(this.intervarID);
+	}
 }
-
-module.exports = {
-    wxTimer: wxTimer,
-    stop:stop
-};
+module.exports = wxtimer;
