2f2587f6a245e21649897293de83dd454c58b5fb
baqihg <541464750@qq.com>
2017-03-22 16:55:19 +0800

    repair bug
    repair bug

--- wxTimer.js
+++ wxTimer.js
@@ -20,15 +20,17 @@ wxtimer.prototype = {
 	        var tmpTime = new Date(that.endTime - 1000 * count++);
 	        //把2011年1月1日日 00：00：00换成数字型，这样就可以直接1s，1s的减，就变成了倒计时，为了看的更明确，又用new date把字符串换回来了
 	        var tmpTimeStr = tmpTime.toString().substr(16,8);//去掉前面的年月日就剩时分秒了
+	        var wxTimerSecond = (tmpTime.getTime() - new Date("1970/01/01 00:00:00").getTime()) / 1000;
 	        self.setData({
 	            wxTimer:tmpTimeStr,
-	            wxTimerSecond:(tmpTime.getTime() - new Date("1970/01/01 00:00:00").getTime()) / 1000
+	            wxTimerSecond:wxTimerSecond,
 	        });
-	        //计时结束
+	        //时间间隔执行函数
 	        if( 0 == (count-1) % that.interval && that.intervalFn){
 	            that.intervalFn();
 	        }
-	        if(tmpTimeStr == "00:00:00"){
+	        //结束执行函数
+	        if(wxTimerSecond <= 0){
 	            if(that.complete){
 	                that.complete();
 	            }
